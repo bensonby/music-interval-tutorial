17471441350b29265f3170b6e892c1189467002b
bensonby <bensonby@gmail.com>
2020-11-26 16:25:12 +0800

    test

--- index.py
+++ index.py
@@ -1,3 +1,24 @@
 import moviepy.editor as mpy
-clip = mpy.TextClip("Hello World!", font="Noto-Sans-CJK-HK", fontsize=30, color="yellow")
-clip.set_duration(5).write_videofile('hello-world.mp4', fps=30)
+
+width = 300
+height = 240
+background = [255, 255, 255]
+font = "Noto-Sans-CJK-HK"
+font_size = {
+    "h1": 18,
+}
+title_color = "yellow"
+title_duration = 2
+
+title = mpy.TextClip(
+    "Identify an interval in 3 simple steps",
+    font=font,
+    fontsize=font_size["h1"],
+    color=title_color,
+).set_duration(title_duration)
+
+background = mpy.ColorClip(size=(width, height), color=background)
+clips = [title, title]
+output = mpy.concatenate_videoclips(clips)
+add_bg = mpy.CompositeVideoClip([background, output])
+add_bg.write_videofile("result.mp4", fps=5)
