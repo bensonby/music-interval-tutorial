0da2d5dd3c1d550632f291407899a216ab0009f6
bensonby <bensonby@gmail.com>
2020-12-17 04:55:30 +0800

    TItle setup

--- index.py
+++ index.py
@@ -1,24 +1,74 @@
 import moviepy.editor as mpy
 
-width = 300
-height = 240
-background = [0, 255, 255]
-font = "Noto-Sans-CJK-HK"
-font_size = {
-    "h1": 18,
+COLOR = {
+    'white': [255, 255, 255],
+    'black': [0, 0, 0],
 }
-title_color = "yellow"
-title_duration = 2
+WIDTH = 720
+HEIGHT = 480
+SIZE = (WIDTH, HEIGHT)
+BACKGROUND = COLOR['white']
+FONT = 'Noto-Sans-CJK-HK'
+FONT_SIZE = {
+    'h1': 32,
+    'h2': 24,
+    'body': 20,
+}
+PADDING = {
+    'h1': 60,
+}
+MARGIN = 60
+FPS = 5
+DURATION = {
+    'title': 4,
+    'step1': {
+        'total': 10,
+        'heading': 2,
+        'description': 3,
+    },
+}
 
-title = mpy.TextClip(
-    "Identify an interval in 3 simple steps",
-    font=font,
-    fontsize=font_size["h1"],
-    color=title_color,
-).set_duration(title_duration)
+def title():
+    return mpy.CompositeVideoClip([mpy.TextClip(
+        'Identify an interval in 3 simple steps',
+        font=FONT,
+        fontsize=FONT_SIZE['h1'],
+        color='black',
+        ).set_duration(DURATION['title']).set_position('center'),
+    ], size=SIZE)
 
-background = mpy.ColorClip(size=(width, height), color=background).set_duration(10)
-clips = [title, title]
-output = mpy.concatenate_videoclips(clips)
-add_bg = mpy.CompositeVideoClip([background, output])
-add_bg.write_videofile("result.mp4", fps=5)
+def step1():
+    duration = DURATION['step1']
+    heading = mpy.TextClip(
+        'Step 1: Count',
+        font=FONT,
+        fontsize=FONT_SIZE['h1'],
+        color='black'
+    ).set_duration(DURATION['step1']['total']).set_position(('center', MARGIN))
+    description = mpy.TextClip(
+        'Count the number of notes (inclusive) ignoring accidentals',
+        font=FONT,
+        fontsize=FONT_SIZE['body'],
+        color='black',
+    ) \
+        .set_duration(duration['total'] - duration['heading']) \
+        .set_start(duration['heading']) \
+        .set_position(('center', MARGIN + PADDING['h1']))
+    return mpy.CompositeVideoClip([
+        heading,
+        description,
+    ], size=SIZE)
+
+def background():
+    duration = DURATION['title'] + DURATION['step1']['total']
+    return mpy.ColorClip(size=SIZE, color=BACKGROUND).set_duration(duration)
+
+content = mpy.concatenate_videoclips([
+    title(),
+    step1(),
+])
+with_bg = mpy.CompositeVideoClip([
+    background(),
+    content,
+])
+with_bg.write_videofile('result.mp4', fps=FPS)
