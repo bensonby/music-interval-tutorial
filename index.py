15e44ce9dcafa3f246e40b5f1edfb8036516eb7e
bensonby <bensonby@gmail.com>
2020-12-18 02:04:59 +0800

    Refine layout and timing of step1

--- index.py
+++ index.py
@@ -17,8 +17,13 @@ FONT_SIZE = {
     'h3': 18,
     'body': 20,
 }
+FONT_BOX = {
+    'timer': (50, 50),
+    'note': (50, 30),
+    'count': (14, 30),
+}
 NUM_EXAMPLES = 3
-EXAMPLE_WIDTH = 140 # added padding
+EXAMPLE_WIDTH = 150 # added padding
 EXAMPLE_HEIGHT = 160 # added padding
 PADDING = {
     'h1': 60,
@@ -27,14 +32,14 @@ PADDING = {
     'small': 5,
 }
 MARGIN = 60
-FPS = 10
+FPS = 15
 DURATION = {
     'title': 4,
     'step1': {
-        'total': 20,
+        'total': 23,
         'heading': 2,
         'description': 3,
-        'example': 5,
+        'example': 6,
     },
 }
 # EXAMPLES
@@ -50,6 +55,9 @@ NUMBER = ['3rd', '5th', '7th']
 def flatten(l): # flatten a list
     return [x for sublist in l for x in sublist]
 
+def empty_clip():
+    return mpy.TextClip(' ', font=FONT, fontsize=1, color='white')
+
 def title():
     return mpy.CompositeVideoClip([mpy.TextClip(
         'Identify an interval in 3 simple steps',
@@ -64,39 +72,48 @@ def create_example(id, duration):
         .set_duration(duration) \
         .set_start(0) \
         .set_position((0, 'center'))
-    example_text_from = mpy.TextClip(
+    note_from = mpy.TextClip(
         INTERVAL_FROM[id],
         font=FONT,
         fontsize=FONT_SIZE['h2'],
         color='black',
+        align='center',
+        method='caption',
+        size=FONT_BOX['note'],
     ) \
         .set_duration(duration - 1) \
         .set_start(1) \
         .set_position(('right', 'bottom'))
-    example_text_to = mpy.TextClip(
+    note_to = mpy.TextClip(
         INTERVAL_TO[id],
         font=FONT,
         fontsize=FONT_SIZE['h2'],
         color='black',
+        align='center',
+        method='caption',
+        size=FONT_BOX['note'],
     ) \
     .set_duration(duration - 1) \
     .set_start(1) \
     .set_position(('right', 'top'))
+
+    notes_vertical_gap = int((EXAMPLE_HEIGHT - (len(BETWEEN_NOTES[id]) + 2) * FONT_BOX['note'][1]) / (len(BETWEEN_NOTES[id]) + 1))
     between_notes = [
         mpy.TextClip(
             note,
             font=FONT,
-            fontsize=FONT_SIZE['h3'],
+            fontsize=FONT_SIZE['h2'],
             color='blue',
+            align='center',
+            method='caption',
+            size=FONT_BOX['note'],
         )
         .set_duration(duration - 2 - (len(BETWEEN_NOTES[id]) - i) * 0.1)
         .set_start(2 + (len(BETWEEN_NOTES[id]) - i) * 0.1)
         .set_position(
             (
                 'right',
-                int(EXAMPLE_HEIGHT / (len(BETWEEN_NOTES[id]) + 3) * (i + 1.5))
-                # original it is len + 2, and i + 1
-                # changed the values for layout adjustment
+                FONT_BOX['note'][1] * (i + 1) + notes_vertical_gap * (i + 1),
             )
         )
         for i, note in enumerate(reversed(BETWEEN_NOTES[id]))
@@ -104,14 +121,39 @@ def create_example(id, duration):
     return mpy.CompositeVideoClip(
         [
             image,
-            example_text_from,
-            example_text_to,
+            note_from,
+            note_to
         ] + between_notes,
         size=(EXAMPLE_WIDTH, EXAMPLE_HEIGHT),
     )
 
+def create_step1_count(id, duration):
+    count = len(BETWEEN_NOTES[id])
+    notes_vertical_gap = int((EXAMPLE_HEIGHT - (count + 2) * FONT_BOX['count'][1]) / (count + 1))
+    count_numbers = [
+        mpy.TextClip(
+            str(i),
+            font=FONT,
+            fontsize=FONT_SIZE['h3'],
+            color='red',
+            align='center',
+            method='caption',
+            size=FONT_BOX['count'],
+        )
+        .set_duration(duration)
+        .set_start(2 + count * 0.1 + 0.5 + 0.15 * i)
+        .set_position(
+            (
+                'center',
+                FONT_BOX['count'][1] * (count + 2 - i) + notes_vertical_gap * (count + 2 - i),
+            )
+        )
+        for i in range(1, count + 3)
+    ]
+    return mpy.CompositeVideoClip(count_numbers, size=(FONT_BOX['count'][0], EXAMPLE_HEIGHT))
 
 def create_answer(id, duration):
+    count = len(BETWEEN_NOTES[id])
     return mpy.TextClip(
         NUMBER[id],
         font=FONT,
@@ -119,7 +161,7 @@ def create_answer(id, duration):
         color='red',
     ) \
     .set_duration(duration) \
-    .set_start(3) \
+    .set_start(2 + count * 0.1 + (count + 2) * 0.15 + 1.5) \
     .set_position((
         'center',
         'bottom',
@@ -148,8 +190,14 @@ def step1():
     width_between_examples = int((WIDTH - MARGIN * 2 - NUM_EXAMPLES * EXAMPLE_WIDTH) / (NUM_EXAMPLES - 1))
     examples = [
         mpy.clips_array([
-            [create_example(id, duration['example'] * (NUM_EXAMPLES - id))],
-            [create_answer(id, duration['example'] * (NUM_EXAMPLES - id))],
+            [
+                create_example(id, duration['example'] * (NUM_EXAMPLES - id)),
+                create_step1_count(id, duration['example'] * (NUM_EXAMPLES - id)),
+            ],
+            [
+                create_answer(id, duration['example'] * (NUM_EXAMPLES - id)),
+                empty_clip(), # dummy clip since each nested list needs to be same size
+            ],
         ])
         .set_position((
             MARGIN + id * (EXAMPLE_WIDTH + width_between_examples),
@@ -177,7 +225,7 @@ def timer():
             color='black',
             align='center',
             method='caption',
-            size=((50, 50)),
+            size=FONT_BOX['timer'],
         )
         .set_duration(1)
         for i in range(total)
